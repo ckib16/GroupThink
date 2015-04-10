9cac917c280faa35fa27255624cb46dafc019dbc
ckib16 <ckib16@gmail.com>
2015-04-09 22:24:50 -0400

    Stripe API - core functions working
    Hooked up Stripe API and got test mode to correctly process. Still have some follow up work to do for chaning user roles after charge sucessful, etc.

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,45 @@
+class ChargesController < ApplicationController
+
+  before_action :membership_price
+
+  def new
+    @stripe_btn_data = {
+      key: "#{ Rails.configuration.stripe[:publishable_key] }",
+      description: "Premium Membership",
+      amount: @premium
+    }
+  end
+
+  def create
+    #Creates a Stripe Customer object, for associating with the charge
+    customer = Stripe::Customer.create(
+      email: current_user.email,
+      card: params[:stripeToken]
+      )
+
+    #Where the real magic happens
+    charge = Stripe::Charge.create(
+      customer: customer.id, #different than user_id
+      amount: @premium,
+      description: "Premium Membership - #{current_user.email}",
+      currency: 'usd'
+      )
+
+    # add code: if current_user is upgraded = change role to premium and flash success; if not = redirect
+
+    flash[:success] = "Thank you for your payment #{current_user.email}!"
+    redirect_to user_path(current_user)
+
+  #Rescue block catches and displays errors
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
+  end
+
+  private
+  def membership_price
+    #price in cents
+    @premium = 1000
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :wikis
+  resources :charges, only: [:new, :create]
 
   devise_for :users
   
