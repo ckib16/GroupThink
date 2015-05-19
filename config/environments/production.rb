50dcfa116c03d5c732d055639f22007197be726d
ckib16 <ckib16@gmail.com>
2015-05-18 22:29:54 -0400

    Fix for Heroku & SendGrid
    set up Env/Production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,4 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'groupthink16.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
 end
