ccb8b43c0395b0a2bc421b6ce78a84d56f0b02a1
ckib16 <ckib16@gmail.com>
2015-05-18 22:13:48 -0400

    Fix for Heroku mailer
    Added corrections to Env/Production
    
    http://stackoverflow.com/questions/4114835/heroku-devise-missing-host-to-link-to-please-provide-host-parameter-or-set-d

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,6 +40,6 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.delivery_method = :stmp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 end
