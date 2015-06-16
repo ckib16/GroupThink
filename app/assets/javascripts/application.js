4392ad41b67b3da4171cadbbf9f4f62f3c65e445
ckib16 <ckib16@gmail.com>
2015-06-15 21:23:47 -0400

    Added QuickMetrics snippet to app
    Used to track page opens on QuickMetrics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,19 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap
+
+//QuickMetrics snippet to put in tracked application
+
+$(document).ready( function(){
+  var _bm_event = {
+    event: { name: "pageLoad"},
+  }
+
+  var _bm_request = new XMLHttpRequest();
+  _bm_request.open("POST", "https://quickmetrics.herokuapp.com//api/events", true);
+  _bm_request.setRequestHeader('Content-Type', 'application/json');
+  _bm_request.onreadystatechange = function() {
+    // respond to controller finish events
+  };
+  _bm_request.send(JSON.stringify(_bm_event));
+});
